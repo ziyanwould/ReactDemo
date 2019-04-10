40d4c91b57a4aff061abf4e910ae1692c1f6d4e9
ziyanwould <448736378@qq.com>
2019-04-10 23:05:11 +0800

    react 生命周期的基本运用

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem'
 import './style.css'
+import axios from 'axios'
 class App extends Component {
   constructor(props){
     super(props);
@@ -16,13 +17,13 @@ class App extends Component {
   }
 
   render() {
-    console.log('parent render')
+   
     return (
       <React.Fragment>
          <div>
          <label htmlFor='insertArea'>输入内容</label>{/**扩大点击区域 */}
           <input 
-            // 曾经经常
+         
             id='insertArea'
             className='input'
             value={this.state.inputValue} 
@@ -32,7 +33,7 @@ class App extends Component {
           />
           <button onClick={this.handleBtnClick}>提交</button>
          </div>
-         <ul ref={(ul)=>{this.ul=ul}}>
+         <ul>
          {this.getTodoItem()}
          </ul>
         
@@ -41,34 +42,12 @@ class App extends Component {
     );
   }
 
-  //在组件即将被挂载到页面时候自动执行 只执行一次 生命周期函数
-  componentWillMount(){
-    console.log('componentWillMount')
-  }
-
-  //在组件即将被挂载到页面后 自动执行 只执行一次 生命周期函数
   componentDidMount(){
-    console.log('componentDidMount')
+    //可以在这里ajax 请求 这里比较好
+    axios.get('/api/todolist')
+    .then(()=>{alert('succ')})
+    .catch(()=>{alert('err')})
   }
-
-  //组件被更新之前，他会自动被执行 生命周期函数
-  shouldComponentUpdate(){
-    console.log('shouldComponentUpdate')
-    return true //true 组件需要更新 false 组件将不会更新 后面流程不会被执行
-  }
-  
-  //组件更新之前，它会自动执行，但是他在shouldComponentUpdate之后执行
-  //如果shouldComponentUpdate 返回true他才执行
-  //如果返回false，这个函数就不会被执行
-  componentWillUpdate(){
-    console.log('componentWillUpdate')
-  }
-  
-  //组件更新完后执行 也就是render函数后执行
-  componentDidUpdate(){
-    console.log('componentDidUpdate')
-  }
-  
 
   getTodoItem(){
     /**代码比较多时候 要进行函数的拆分 不要杂糅一起 */
@@ -77,7 +56,7 @@ class App extends Component {
       
          
         <TodoItem
-         key={item}
+         key={index}
          count={item}
          index={index}
          /**强制绑定父组件的this 不然子组件找不到父组件的方法 */
@@ -90,11 +69,7 @@ class App extends Component {
     })
   }
   changeinput(e){
-    //console.log(e.target.value)
-    // let list = [...this.state.list,e.target.inputValue];
-    // this.state({
-    //   list:list
-    // })
+
 
     //旧版的更改数据的方法
     // this.setState({
@@ -119,10 +94,7 @@ class App extends Component {
       list:[...prevState.list,prevState.inputValue],
       inputValue:''
     }
-   ),()=>{
-     /**由于setstate 是一个异步函数 应该执行完后再进行DOM节点操作 */
-    console.log(this.ul.querySelectorAll('div').length)
-   })
+   ))
    
   }
   handleItemDelete(index){
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,17 +5,27 @@ class TodoItem extends Component{
      super(props);
      this.handleClick = this.handleClick.bind(this);//在这里绑定this 有利于性能优化
     }
+
+    shouldComponentUpdate(nextProps,nextState){
+        //提升性能 数组有更改之后再重新渲染 避免不必要虚拟DOM浪费
+        if(nextProps.count!== this.props.count){
+            return true
+        }else{
+            return false
+        }
+        
+    }
     render(){
-        console.log('child render')
+      
         /**存一个属性变量值 {}花括号的的结构赋值*/
-        const { count,text } = this.props
+        const { count} = this.props
         /**JSX --> createElemnt --> 虚拟DOM (JS对象 )--> 真实的DOM*/
         return (
             <div  
         
             onClick={this.handleClick}
             >
-              {text}  - {count}
+              {count}
             </div>
             )
     }
@@ -27,33 +37,16 @@ class TodoItem extends Component{
         deleteItem(index);
        //  this.props.deleteItem(this.props.index)
     }
-    //当一个组件从父组件接受参数
-    //只要父组件的render函数被重新执行，子组件的生命周期函数就会被执行
 
-    //或者说
-    //如果这个组件第一次存在于父组件中，不会执行
-    //如果这个组件之前已经存在于父组件中 ，才行执行
-    componentWillReceiveProps(){
-        console.log('child componentWillReceiveProps')
-    }
-
-      //组件被移除前执行
-  componentWillUnmount(){
-    console.log('child componentWillUnmount')
-  }
 
     
 }
 //propTypes 对父组件传过来的数据类型加以核验 类型校验
 TodoItem.propTypes ={
-    text:PropTyprs.string.isRequired,//必须要传数值 且不能为空
     cont:PropTyprs.oneOfType([PropTyprs.string,PropTyprs.number]),//可以是字符串或数字
     deleteItem:PropTyprs.func,
     index:PropTyprs.number
 
 }
-//如果父组件没有传数据 输出默认数据
-TodoItem.defaultProps={
-    text:'hello world'
-}
+
 export default TodoItem;
