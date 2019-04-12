d88e9e32cac930b26f81d3d77ca2efc81ba53e2e
ziyanwould <448736378@qq.com>
2019-04-12 18:02:25 +0800

    react-transition-group 多个动画

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React,{ Component} from 'react'
-import { CSSTransition } from 'react-transition-group';
+import {
+    CSSTransition,
+    TransitionGroup,
+  } from 'react-transition-group';
 import './style.css'
 class App extends Component {
     constructor(props){
         super(props);
         this.state ={
-            show:true
+            list:[]
         }
-        this.handleToggle= this.handleToggle.bind(this)
+        this.handleItem= this.handleItem.bind(this)
     }
     render(){
 
         return(
             <React.Fragment>
-                <CSSTransition
-                 in={this.state.show}
-                 timeout={1000}
-                 classNames='fade'
-                 unmountOnExit
-                 onEntered={(el) => {el.style.color='blue'}}//钩子函数 在某个时刻自动执行的函数 比如onEntered是当入场动画执行后
-                 appear={true}//第一次加载时候 也要有动画
-                >
-                 <div>hellow</div>
-                </CSSTransition>
-                {/*<div className={this.state.show?'show':'hide'}>hellow</div>*/}
-                <button onClick={this.handleToggle}>toggle</button>
+                  <TransitionGroup >
+                {
+                   this.state.list.map((item,index) =>{
+                       return(
+                        <CSSTransition
+                       
+                        timeout={1000}
+                        classNames='fade'
+                        unmountOnExit
+                        onEntered={(el) => {el.style.color='blue'}}//钩子函数 在某个时刻自动执行的函数 比如onEntered是当入场动画执行后
+                        appear={true}//第一次加载时候 也要有动画
+                        key={index}
+                       >
+                       <div >{item}</div>
+                         </CSSTransition>
+                           
+                       )
+                   })
+                }
+              
+              
+                <button onClick={this.handleItem}>toggle</button>
+                </TransitionGroup>
             </React.Fragment>
         ) 
     }
-    handleToggle(){
-      this.setState({
-         show:this.state.show ? false : true
-      })  
+    handleItem(){
+     this.setState((prevState) => {
+         return{
+             list : [...prevState.list,'item']
+         }
+     })
     }
 }
 
